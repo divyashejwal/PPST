66d9357f626990b608ff2d090ce9de14ef6ce2b7
divyashejwal <divya.shejwal24@sanjivani.edu.in>
2024-09-06 21:21:19 +0530

    Add files via upload

---
+++ main(7).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=6,b=4;
+  
+  //Equal operator
+  cout<<"a==b is"<<(a==b)<<endl;
+  
+  //Greter than operator
+  cout<<"a>b is"<<(a>b)<<endl;
+  
+  //Greter than or equel to operator
+  cout<<"a>=b is"<<(a>=b)<<endl;
+  
+  //Lessor than operator
+  cout<<"a<b is"<<(a<b)<<endl;
+  
+  //Lessor than or equal to operator
+  cout<<"a<=b is"<<(a<=b)<<endl;
+  
+  //truecout<<"a!=b is"<<(a!=b)<<endl;
+  return 0;
+  }
