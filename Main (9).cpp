804cde76cccd6c31bfa3d0ac6188906d02606a6f
divyashejwal <divya.shejwal24@sanjivani.edu.in>
2024-09-06 21:28:59 +0530

    Add files via upload

---
+++ Main (9).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a=2,b=4
+  
+  //Binary AND operator
+  cout<<"a&b is"<<(2&4)<<endl;
+  
+  //Binary OR operator
+  cout<<"a/b is"<<(2/4)<<endl;
+  
+  //Binary XOR operator
+  cout<<"a^b is"<<(2^4)<<endl;
+  
+  //Left shift operator
+  cout<<"a<<1 is"<<(a<<l)<<endl;
+  
+  //Right shift operator
+  cout<<"a>>1 is"<<(a>>1)<<endl;
+  
+  //one's complement operator
+  cout<<"~(a)is"<<~(a)<<endl;
+  
+  return 0;
+}
