899369e08cc3aacff26a32bf88df5fffc75612fc
divyashejwal <divya.shejwal24@sanjivani.edu.in>
2024-09-06 20:57:30 +0530

    Add files via upload

---
+++ main(6).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=2,b=4;
+  
+  //Binary AND operator
+  cout<<"a&b is"<<(a&b)<<endl;
+  
+  //Binary OR operator
+  cout<<"a|b is"<<(a|b)<<endl;
+  
+  //Modulo operator
+  cout<<"a%b="<<(a%b)<<endl;
+  
+  return 0;
+}
