44927b7b4ac2a5b5663c14931ab8f2b1a8592307
divyashejwal <divya.shejwal24@sanjivani.edu.in>
2024-09-23 17:32:34 +0530

    Add files via upload

---
+++ Main (14) Rational operator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a=6,b=4;
+    
+    //Equal operator
+    cout<<"a==b is"<<(a==b)<<endl;
+    
+    //Greater than operator
+    cout<<"a>b is"<<(a>b)<<endl;
+    
+    //Greater than or equal to opeartor
+    cout<<"a>=b is"<<(a>=b)<<endl;
+    
+    //Lesser operator
+    cout<<"a<b is"<<(a<b)<<endl;
+    
+    //Lesser than or eqaul to operator
+    cout<<"a<=b is"<<(a<=b)<<endl;
+    
+    //true
+    cout<<"a!=b is"<<(a!=b)<<endl;
+    
+    return 0;
+}
