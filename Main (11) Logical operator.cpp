656a070f07e2504db0a6a75e8cc70be5455d41db
divyashejwal <divya.shejwal24@sanjivani.edu.in>
2024-09-23 17:20:51 +0530

    Add files via upload

---
+++ Main (11) Logical operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+ int a=6,b=4;
+//Logical AND operatorcout<<"a&&b is"<<(a&&b)<<endl;
+
+//Logical OR operator
+cout<<"a||b is"<<(a||b)<<endl;
+
+//Logical NOT operator
+cout<<"~a is"<<~(a)<<endl;
+
+return 0;
+}
